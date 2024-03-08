b3a4d1f36edf5ac42937f8f32937589f24ba6c6d
fivge <luanxingtong@gmail.com>
2024-03-08 15:28:03 +0000

    feat: the graph

--- README.md
+++ README.md
@@ -30,3 +30,23 @@ yarn hardhat ignition deploy ignition/modules/Apollo.ts --network localhost
 yarn hardhat ignition deploy ignition/modules/MToken.ts --network sepolia
 yarn hardhat run scripts/deploy-simpletoken.ts  --network sepolia
 ```
+
+## 05 ~~The Graph~~
+
+https://thegraph.com/docs/zh/quick-start/
+
+```bash
+graph init tg-02 tg-02 --protocol ethereum --network sepolia --abi "./ignition/deployments/chain-11155111/artifacts/MTOKEN#SimpleToken.json" --from-contract 0xF887Ac1b01439475bD75904359A4CB741E640b93 --start-block 5441889 --studio
+```
+
+```bash
+graph auth --studio
+
+graph codegen && graph build
+
+graph deploy --studio tg-01
+```
+
+Error: Failed to upload file to IPFS: fetch failed
+
+**GIVE UP**
--- tg-02/src/contract.ts
+++ tg-02/src/contract.ts
@@ -1,30 +1,49 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { Contract, Approval, Transfer } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { BigInt } from "@graphprotocol/graph-ts";
+import { Contract, Approval, Transfer } from "../generated/Contract/Contract";
+import { TransferEntity, ApprovalEntity } from "../generated/schema";
 
 export function handleApproval(event: Approval): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from)
+  let entity = ApprovalEntity.load(event.transaction.from.toHex());
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from)
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  if (entity == null) {
+    entity = new ApprovalEntity(event.transaction.from.toHex());
   }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  entity.value = event.params.value;
 
   // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
+  entity.owner = event.params.owner;
+  entity.spender = event.params.spender;
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  entity.save();
+}
+
+export function handleTransfer(event: Transfer): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = TransferEntity.load(event.transaction.from.toHex());
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (entity === null) {
+    entity = new TransferEntity(event.transaction.from.toHex());
+  }
+
+  // BigInt and BigDecimal math are supported
+  entity.value = event.params.value;
+
+  // Entity fields can be set based on event parameters
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+
+  // Entities can be written to the store with `.save()`
+  entity.save();
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -52,5 +71,3 @@ export function handleApproval(event: Approval): void {
   // - contract.transfer(...)
   // - contract.transferFrom(...)
 }
-
-export function handleTransfer(event: Transfer): void {}
